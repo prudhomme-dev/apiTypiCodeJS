ae76093351ee5267110aa286b70dbc1ecb1cf973
Cedric P <cedric.prudhomme@protonmail.com>
2022-02-24 16:53:39 +0100

    Ajout de l'affichage des tâches

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+let usertask = {
+    template: `
+    <section>
+        <h3>Tâche à réaliser pour l'utilisateur {{username}}</h3>
+        <button v-on:click="hiddenTaskCompleted()">Cacher les tâches déjà réalisées</button>
+        <ul>
+            <li v-for="(task, index) of userTask" >
+            <span v-if="task.completed" class="task completedTask">{{task.title}}</span>
+            <span v-else class="task uncompletedTask">{{task.title}}</span>
+            - <button v-on:click="deleteTask($event)" class="deleteTask" :data-index="index">Supprimer</button>
+            </li>
+
+        </ul>
+
+    </section>
+    
+    `,
+    data: function () {
+        return {
+            userTask: []
+        }
+    },
+    props: ['iduser', 'username'],
+    created: function () {
+        this.tasksList();
+    },
+    methods: {
+        tasksList: async function () {
+            try {
+                let response = await fetch('https://jsonplaceholder.typicode.com/todos?userId=' + this.iduser);
+                let tasks = await response.json();
+                this.userTask = tasks;
+            } catch (e) {
+                console.error('ERREUR', e);
+            }
+        },
+        hiddenTaskCompleted: function () {
+            let taskCompleted = document.querySelectorAll(".completedTask")
+            for (task of taskCompleted) {
+                task.parentElement.classList.toggle("hiddenTask")
+            }
+        },
+        deleteTask: function (e) {
+            this.userTask.splice(e.target.dataset.index, 1)
+        }
+    }
+}
+
 let userdata = {
     template: `<section>
         <h3>{{datauser.name}} ({{datauser.username}})</h3>
@@ -12,25 +60,42 @@ let userdata = {
         </p>
         
         <p>Entreprise : {{datauser.company.name}}</p>
+        <p>
+            <button v-on:click="activeDisplayTask()">Voir les tâches</button>
+            <button>Voir les albums</button>
+            <button>Voir les articles</button>
+        </p>
+
+        <usertask v-if="displayTask" :iduser="idUser" :username="datauser.name"></usertask>
+
     </section>
 
     `,
+    created: function () {
+        this.idUser = this.datauser.id;
+    },
+    updated: function () {
+        this.idUser = this.datauser.id;
+    },
+    components: {
+        usertask
+
+    },
+    data: function () {
+        return {
+            idUser: 0,
+            displayTask: false
+        }
+    },
     props: ['datauser'],
     filters: {
         webSite: function (value) {
             return "https://" + value
         }
-
     },
     methods: {
-        loadUser: async function () {
-            try {
-                let response = await fetch('https://jsonplaceholder.typicode.com/users?id=' + this.iduser);
-                let user = await response.json();
-                this.datauser = user;
-            } catch (e) {
-                console.error('ERREUR', e);
-            }
+        activeDisplayTask: function () {
+            this.displayTask = !this.displayTask;
         }
     }
 }
